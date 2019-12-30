6620f06b490038af95410220ec3d9aef0faaf35d
Josh Nice <josh.nice@iventis.co.uk>
2019-12-30 11:12:52 +0000

    changed the sidebar to a seperate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Index } from './index/index.component';
+import { SideBarNavigation } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Index
+    Index,
+    SideBarNavigation
   ],
   imports: [
     BrowserModule
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sidebar-navigation',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+
+export class SideBarNavigation {
+}
