077f4dcbdca16113a2f0bf8edf3e228854daad15
Josh Nice <josh.nice@iventis.co.uk>
2019-12-30 15:11:40 +0000

    second iteration of the sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,16 +6,21 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 
-
-
 export class SideBarNavigation {
+
     @Output() sidebar_state_change: EventEmitter<boolean> = new EventEmitter();
     public sidebar_expanded: boolean = false;
+    public background_colour: string = 'transparent';
     
-    sidebarClick() {
-      console.log('sidebar clicked is called');
+
+    public sidebarClick() {
         this.sidebar_expanded = !this.sidebar_expanded;
         this.sidebar_state_change.emit(this.sidebar_expanded);
+        this.checkBackgroundColour();
     }
 
+    private checkBackgroundColour() {
+      this.sidebar_expanded ? this.background_colour = 'black' : this.background_colour = 'transparent';
+      console.log('background colour', this.background_colour);
+    }
 }
