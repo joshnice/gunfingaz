a6392a18decdfaaa18e71100493ee15bfd188c68
Josh Nice <josh.nice@iventis.co.uk>
2019-12-30 13:45:09 +0000

    first iteration of sidebar

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 
 export class Index {
+
+  public sidebar_top_css: number = 20;
+  public sidebar_left_css: number = 20;
+
+
+  public sidebarStateChange(event: any) {
+    if (event === true){
+      this.sidebar_top_css = 0;
+      this.sidebar_left_css = 0;
+    } else {
+      this.sidebar_top_css = 20;
+      this.sidebar_left_css = 20;
+    }
+  }
+
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'sidebar-navigation',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 
+
+
 export class SideBarNavigation {
+    @Output() sidebar_state_change: EventEmitter<boolean> = new EventEmitter();
+    public sidebar_expanded: boolean = false;
+    
+    sidebarClick() {
+      console.log('sidebar clicked is called');
+        this.sidebar_expanded = !this.sidebar_expanded;
+        this.sidebar_state_change.emit(this.sidebar_expanded);
+    }
+
 }
